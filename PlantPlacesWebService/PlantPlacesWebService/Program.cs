ec17891988b597c22ca4de48884efa5e5a42320e
alsamh <alsmh76@gmail.com>
2022-07-06 15:16:19 +0300

    just witespace

--- PlantPlacesWebService/PlantPlacesWebService/Program.cs
+++ PlantPlacesWebService/PlantPlacesWebService/Program.cs
@@ -20,5 +20,6 @@ namespace PlantPlacesWebService
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+          
     }
 }
